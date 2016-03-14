78882e75d2ddb567fe8b7fecd06a19f9a1ab3afb
James Peterson <James.Peterson@nih.gov>
2016-03-14 16:17:46 -0400

    First Update

---
+++ RespWebBasics/Week 4/project/myjs.js
@@ -0,0 +1,10 @@
+// <!--
+//  <!-- include jquery, bootstrap and our stylesheet -->
+//  <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+// -->
+
+//  <script type="text/javascript">
+    $("#mytitle").text("My Page");
+    $("#tophdr").text("My Header");
+//  </script>
+
