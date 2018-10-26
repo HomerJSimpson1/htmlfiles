021ec753a75f807038d5298bbcc63d6126e09a67
James Peterson <james@recruitcom.com>
2018-10-26 09:19:04 -0400

    Latest updates using sessions

---
+++ PMJText/php/session_recall.php
@@ -0,0 +1,19 @@
+<?php
+	session_start();
+?>
+
+<?DOCTYPE html>
+
+<html language="en">
+   <head>
+	<title>Your Products</title>
+   </head>
+   <body>
+	<h1>Your Products</h1>
+	<p>Your chosen products are:</p>
+	<ul>
+		<li><?php echo $_SESSION['product1']; ?></li>
+		<li><?php echo $_SESSION['product2']; ?></li>
+	</ul>
+   </body>
+</html>
