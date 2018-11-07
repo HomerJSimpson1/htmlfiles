0dbeadc793532f355806950812f9540a267e1288
Peterson <james.peterson@nih.gov>
2018-11-06 20:07:10 -0500

    Latest updates, added count.php

---
+++ PMJText/php/count.php
@@ -0,0 +1,24 @@
+<?php
+	$mysqli = mysqli_connect("localhost", "testuser", "somepass", "testDB");
+
+	if (mysqli_connect_errno()) {
+	   printf("Connect failed: %s\n", mysqli_connect_errno());
+	   exit();
+	}
+	else {
+	   $sql = "SELECT * FROM testTable";
+	   $res = mysqli_query($mysqli, $sql);
+
+	   if ($res) {
+	      $number_of_row = mysqli_num_rows($res);
+	      printf("Result set has %d rows.\n", $number_of_rows);
+	   }
+	   else {
+	      printf("Could not retrieve records: %s\n", mysqli_error($mysqli));
+	   }
+
+	   mysqli_free_result($res);
+	   mysqli_close($mysqli);
+	}
+
+?>
