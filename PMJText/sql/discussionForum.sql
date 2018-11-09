0207faafac1428980c180caedf74d385a9944e06
James Peterson <james@recruitcom.com>
2018-11-08 21:03:49 -0500

    Latest updates

---
+++ PMJText/sql/discussionForum.sql
@@ -0,0 +1,15 @@
+CREATE TABLE forum_topics (
+       	     topic_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       	     topic_title VARCHAR(150),
+	     topic_create_time DATETIME,
+	     topic_owner VARCHAR(150)
+	     );
+
+CREATE TABLE forum_posts (
+       	     post_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	     topic_id INT NOT NULL,
+	     post_text TEXT NOT NULL,
+	     post_create_time DATETIME,
+	     post_owner VARCHAR(150),
+	     FOREIGN KEY (topic_id) REFERENCES forum_topics (topic_id)
+	     );
